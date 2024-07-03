74b34644240096f70f648882beee3829f8be3b81
swara <swara.kubade@gmail.com>
2024-07-03 21:38:47 +0530

    Created classes Employee,FullTimeEmployee and PartTimeEmployee

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,55 @@
+abstract class Employee{
+    private String name;
+    private int id;
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public abstract double CalculateSalary();
+
+    @Override
+    public String toString(){
+        return "Employee[name="+name+", id="+id+", salary="+CalculateSalary()+"]";
+    }
+}
+
+class FullTimeEmployee extends Employee{
+
+
+    private double monthlySalary;
+    public FullTimeEmployee(String name, int id, double monthlySalary){
+        super(name,id);
+        this.monthlySalary= monthlySalary;
+
+    }
+    @Override
+    public double CalculateSalary(){
+      return monthlySalary;
+    }
+}
+
+class PartTimeEmployee extends Employee{
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate){
+        super(name, id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate= hourlyRate;
+    }
+
+    @Override
+    public double CalculateSalary(){
+        return hoursWorked*hourlyRate;
+    }
+}
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
